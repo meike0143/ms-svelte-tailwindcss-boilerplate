b10c4641c5647d99722c03b7848d176c9ea52fb4
Rimmert Zelle <rimmert.zelle@gmail.com>
2024-08-23 16:22:08 +0200

    Added some components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,13 @@
 </script>
 
 <div class="flex flex-col h-screen justify-between">
+	<header><p class="bg-red-50 p-2">&#128013 Veterinarian complete</p></header>
 	<main>
 		<slot />
+		<!-- here the page content of +page.svelte is loaded-->
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p class="p-2 text-center">Made with &#x2764;</p>
 	</footer>
 </div>
-
-<style>
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,37 @@
 <script>
-	import Example from '$lib/components/Example.svelte';
-
-</script>
-
-<div class="container w-full mx-auto">
-    <h1>Welcome to Sveltekit</h1>
-    <Example />
-</div>
-
-<style>
-</style>
+    import Appointment from '$lib/components/Appointments.svelte';
+    import Clients from '$lib/components/Clients.svelte';
+    import ClientDetails from '$lib/components/ClientDetails.svelte';
+  </script>
+  
+  <svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Veterinarian - part 1" />
+  </svelte:head>
+  
+  <div class="container w-full mx-auto">
+    <section class="mt-10">
+      <h2 class="text-6xl">Appointments</h2>
+    </section>
+    <section class="flex flex-col lg:flex-row mt-5">
+      <div class="lg:basis-4/6 bg-slate-50 rounded-lg p-10 lg:mr-8">
+        <Appointment />
+      </div>
+      <div class="lg:basis-2/6 bg-slate-50 rounded-lg p-10 sm:mt-4 lg:mt-0">
+        <ClientDetails />
+      </div>
+    </section>
+  </div>
+  <div class="container w-full mx-auto mt-4">
+    <section class="mt-4">
+      <h2 class="text-4xl">Clients</h2>
+    </section>
+    <section class="flex flex-col lg:flex-row mt-5">
+      <div class="lg:basis-4/6 bg-slate-50 rounded-lg p-10 lg:mr-8">
+        <Clients />
+      </div>
+    </section>
+  </div>
+  
+  <style>
+  </style>
