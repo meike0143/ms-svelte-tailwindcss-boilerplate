0cf5b211213fd4a4f1156169bedbe0cfb30dcaf4
Rimmert Zelle <rimmert.zelle@gmail.com>
2024-08-23 17:19:11 +0200

    Revert "Added some components"
    This reverts commit b10c4641c5647d99722c03b7848d176c9ea52fb4.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,14 @@
 </script>
 
 <div class="flex flex-col h-screen justify-between">
-	<header><p class="bg-red-50 p-2">&#128013 Veterinarian complete</p></header>
 	<main>
 		<slot />
-		<!-- here the page content of +page.svelte is loaded-->
 	</main>
 
 	<footer>
-		<p class="p-2 text-center">Made with &#x2764;</p>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 </div>
+
+<style>
+</style>
